ee9b1f41c5e82aaa4e145eff802bc67bfb7fb8fd
unknown <jsafroze@gmail.com>
2024-08-13 01:02:41 -0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Next js 14 Ecommerce',
+  title: 'CPZeeba',
   description: 'Modern Ecommerce Website',
 }
 
