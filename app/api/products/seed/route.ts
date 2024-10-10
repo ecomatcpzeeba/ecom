485881271358c80661a137020bed65e196d722c2
Emirates-car <emiratesautomobileparts@gmail.com>
2024-10-09 22:31:38 -0700

    commit

--- app/api/products/seed/route.ts
+++ app/api/products/seed/route.ts
@@ -5,16 +5,26 @@ import UserModel from '@/lib/models/UserModel'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (request: NextRequest) => {
+  // Check if we are not in production
+  if (process.env.NODE_ENV === 'production') {
+    return NextResponse.json({
+      message: 'Seeding skipped in production',
+    })
+  }
+
   const { users, products } = data
   await dbConnect()
+
+  // Seeding Users
   await UserModel.deleteMany()
   await UserModel.insertMany(users)
 
+  // Seeding Products
   await ProductModel.deleteMany()
   await ProductModel.insertMany(products)
 
   return NextResponse.json({
-    message: 'seeded successfully',
+    message: 'Seeded successfully',
     users,
     products,
   })
