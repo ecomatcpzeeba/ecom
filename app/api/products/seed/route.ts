f9ddc014c5c380d8ab7c40600568c546e6aa44fd
Emirates-car <emiratesautomobileparts@gmail.com>
2024-10-09 22:34:41 -0700

    commit

--- app/api/products/seed/route.ts
+++ app/api/products/seed/route.ts
@@ -5,13 +5,6 @@ import UserModel from '@/lib/models/UserModel'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (request: NextRequest) => {
-  // Check if we are not in production
-  if (process.env.NODE_ENV === 'production') {
-    return NextResponse.json({
-      message: 'Seeding skipped in production',
-    })
-  }
-
   const { users, products } = data
   await dbConnect()
 
