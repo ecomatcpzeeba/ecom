48a2d1a968d6a21e255f97b1e04af4e26d659960
Emirates-car <emiratesautomobileparts@gmail.com>
2024-10-20 19:41:27 -0700

    commit

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,206 @@
+import React from 'react'
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <div className="flex justify-center">
+        <div className="w-2/3">
+          <h1 className="text-bold text-4xl text-center m-4">Privacy Policy</h1>
+          <p className="my-4 font-bold">Last updated on October 20 2024</p>
+          <p>
+            At <b>cpzeeba.com</b>, we value your privacy and are committed to
+            protecting your personal information. This Privacy Policy outlines
+            how we collect, use, share, and protect your data when you shop on
+            our website. By using our services, you agree to the terms of this
+            Privacy Policy.
+          </p>
+          <div>
+            <h6 className="text-2xl font-bold">1. Information We Collect</h6>
+            <p>
+              We collect various types of personal data to provide and improve
+              our services. The types of information we collect include:
+            </p>
+            <ul className="list-disc">
+              <li>
+                1. <b>Personal Identification Information:</b> Name, email
+                address, shipping address, phone number, and account login
+                credentials.
+              </li>
+              <li>
+                2. <b>Payment Information:</b> Credit/debit card details,
+                billing address, and other payment-related information.
+              </li>
+              <li>
+                3. <b>Shopping Data:</b> Purchase history, product preferences,
+                and any feedback or reviews provided.
+              </li>
+              <li>
+                4. <b>Automatically Collected Data:</b> Your IP address, browser
+                type, and device information when you visit our website.
+              </li>
+              <li>
+                5. <b>Cookies:</b> Information from cookies and other tracking
+                technologies to enhance your shopping experience.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h6 className="text-2xl font-bold">
+              2. How We Use Your Information
+            </h6>
+            <p>We use the information we collect for the following purposes:</p>
+            <ul className="list-disc">
+              <li>
+                1. <b>Order Processing:</b> To fulfill your purchases, including
+                processing payments and shipping products to you.
+              </li>
+              <li>
+                2. <b>Customer Support: </b> To respond to your queries and
+                provide assistance with your orders.
+              </li>
+              <li>
+                3. <b>Personalization:</b> To tailor your shopping experience
+                based on your preferences and past purchases.
+              </li>
+              <li>
+                4. <b>Marketing:</b> To send you promotional emails,
+                newsletters, and special offers (only with your consent).
+              </li>
+              <li>
+                5. <b>Security:</b> To protect our website and your data from
+                fraud and unauthorized access.
+              </li>
+              <li>
+                5. <b>Legal Compliance:</b> To comply with applicable laws,
+                regulations, and legal processes.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h6 className="text-2xl font-bold">3. Sharing Your Information</h6>
+            <p>
+              We only share your personal information with third parties when
+              necessary to provide our services or comply with legal
+              obligations. This includes:
+            </p>
+            <ul className="list-disc">
+              <li>
+                1. <b>Payment Processors: </b> Secure third-party payment
+                gateways like Stripe or PayPal to process your payments.
+              </li>
+              <li>
+                2. <b>Shipping Providers:</b> Courier services to deliver your
+                purchases (e.g., DHL, FedEx).
+              </li>
+              <li>
+                3. <b> Marketing Partners:</b> Third-party platforms that help
+                us send promotional emails and personalized offers (e.g.,
+                Mailchimp).
+              </li>
+              <li>
+                4. <b>Legal Authorities:</b> If required to comply with a legal
+                request or obligation.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h6 className="text-2xl font-bold">
+              4. How We Protect Your Information
+            </h6>
+            <p>
+              We take appropriate security measures to protect your data,
+              including:
+            </p>
+            <ul className="list-disc">
+              <li>
+                1. <b>Encryption:</b> We use Secure Socket Layer (SSL)
+                technology to encrypt sensitive data like payment details during
+                transmission.
+              </li>
+              <li>
+                2. <b>Secure Servers: </b>We store your information on secure
+                servers protected by firewalls.
+              </li>
+              <li>
+                3. <b> Access Control:</b> Only authorized personnel have access
+                to your data.
+              </li>
+            </ul>
+            <p>
+              While we strive to use commercially acceptable means to protect
+              your personal information, we cannot guarantee its absolute
+              security.
+            </p>
+          </div>
+          <div>
+            <h6 className="text-2xl font-bold">
+              5. Cookies and Tracking Technologies
+            </h6>
+            <p>
+              We use cookies and similar technologies to collect information
+              about your browsing behavior on cpzeeba.com. These help us improve
+              your shopping experience by:
+            </p>
+            <ul className="list-disc">
+              <li>Remembering your preferences and shopping cart contents.</li>
+              <li>Analyzing website traffic and performance.</li>
+              <li>Personalizing product recommendations.</li>
+            </ul>
+            <p>
+              You can manage or disable cookies in your browser settings, but
+              please note that some parts of our website may not function
+              properly if you do so.
+            </p>
+          </div>
+          <div>
+            <h6 className="text-2xl font-bold">6. Your Rights</h6>
+            <p>You have the right to:</p>
+            <ul className="list-disc">
+              <li>
+                1. <b>Access: </b> Request a copy of the personal information we
+                hold about you.
+              </li>
+              <li>
+                2. <b>Correction: </b>Request that we correct or update any
+                inaccurate or incomplete information.
+              </li>
+              <li>
+                3. <b> Deletion:</b> Request that we delete your personal data,
+                subject to certain legal exceptions.
+              </li>
+              <li>
+                <b>Opt-Out:</b> Unsubscribe from promotional emails and
+                marketing communications at any time.
+              </li>
+            </ul>
+            <p>
+              To exercise any of these rights, please contact us at
+              ecom.chennaiplaza@gmail.com .
+            </p>
+          </div>
+          <div>
+            <h6 className="text-2xl font-bold">
+              7. Changes to This Privacy Policy
+            </h6>
+
+            <p>
+              We may update this Privacy Policy periodically to reflect changes
+              in our practices or legal requirements. Any updates will be posted
+              on this page, and significant changes will be communicated to you
+              via email or a notice on our website.
+            </p>
+          </div>
+          <div>
+            <h6 className="text-2xl font-bold">8. Contact Us</h6>
+
+            <p>
+              If you have any questions or concerns about this Privacy Policy or
+              how we handle your personal data, please contact us at: Email:
+              ecom.chennaiplaza@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
